780a766092bd53f643e9c57d330dd8d9b2cfe780
DevadarshiniK <devadarshini1603@gmail.com>
2026-04-14 11:51:42 +0530

    Module:Eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -8,13 +8,46 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  enable_irsa = true 
+  enable_irsa = true
 
   # Control plane access
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
+  # 🔥 VERY IMPORTANT (fixes kubectl access)
   enable_cluster_creator_admin_permissions = true
+
+  # =========================
+  # 🔥 NODE GROUP (SPOT)
+  # =========================
+  eks_managed_node_groups = {
+
+  # 💸 Spot (Cost Optimization)
+  spot_nodes = {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+
+    instance_types = [
+      "t3.medium",
+      "t3a.medium",
+      "t3.small",
+      "t3a.small"
+    ]
+
+    capacity_type = "SPOT"
+
+    ami_type = "AL2023_x86_64_STANDARD"
+
+    labels = {
+      node_type = "spot"
+    }
+
+    tags = {
+      Name = "${var.cluster_name}-spot-nodes"
+    }
+  }
+}
 
   tags = var.tags
 }
